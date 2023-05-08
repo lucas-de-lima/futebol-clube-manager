66533ed939852a59f757e21f0789f60f65483dda
Lucas-De-Lima <lucasramosdelima@gmail.com>
2023-05-08 19:35:35 -0300

    test: 50% dos testes de integração

--- app/backend/src/tests/loginIntegration.test.ts
+++ app/backend/src/tests/loginIntegration.test.ts
@@ -1,68 +1,89 @@
-import * as sinon from 'sinon';
-import * as chai from 'chai';
+import * as sinon from "sinon";
+import * as chai from "chai";
+import * as jwt from "jsonwebtoken";
+
 // @ts-ignore
-import chaiHttp = require('chai-http');
+import chaiHttp = require("chai-http");
 
-import { app } from '../app';
-import Example from '../database/models/ExampleModel';
+import { app } from "../app";
+import Example from "../database/models/ExampleModel";
 
-import { Response } from 'superagent';
+import { Response } from "superagent";
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Teste da rota de Login', () => {
-    afterEach(sinon.restore)
+const mockToken =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoyLCJ1c2VybmFtZSI6IlVzZXIiLCJyb2xlIjoidXNlciIsImVtYWlsIjoidXNlckB1c2VyLmNvbSIsInBhc3N3b3JkIjoiJDJhJDA4JFk4QWJpOGpYdnNYeXFtLnJtcDBCLnVRQkE1cVV6N1Q2R2hsZy9DdlZyL2dMeFlqNVVBWlZPIn0sImlhdCI6MTY4MzU1NDI3MywiZXhwIjoxNjg0MTU5MDczfQ.0cYBEnAPk4iSf0DMqtu-_wEERmUSppQjQiLj533jxuw";
 
-    it('Se não existir o campo e-mail deve falhar', async () => {
-        const {status, body} = await chai.request(app).post('/login').send({password: 'secret_admin'})
-        expect(status).to.be.equal(401)
-        expect(body).to.be.deep.equal({ "message": "Invalid email or password" })
-      });
+describe("Teste da rota de Login", () => {
+  afterEach(sinon.restore);
 
-      it('Se o campo e-mail inválido deve falhar', async () => {
-        const {status, body} = await chai.request(app).post('/login').send({password: 'secret_admin', email: 'userInvalid@user.com'})
-        expect(status).to.be.equal(401)
-        expect(body).to.be.deep.equal({ "message": "Invalid email or password" })
-      });
+  it("Se não existir o campo e-mail deve falhar", async () => {
+    const { status, body } = await chai
+      .request(app)
+      .post("/login")
+      .send({ password: "secret_admin" });
+    expect(status).to.be.equal(401);
+    expect(body).to.be.deep.equal({ message: "Invalid email or password" });
+  });
 
-      it('Se o campo password inválido deve falhar', async () => {
-        const {status, body} = await chai.request(app).post('/login').send({password: 'invalid_password', email: 'user@user.com'})
-        expect(status).to.be.equal(401)
-        expect(body).to.be.deep.equal({ "message": "Invalid email or password" })
-      });
+  it("Se o campo e-mail for inválido deve falhar", async () => {
+    const { status, body } = await chai
+      .request(app)
+      .post("/login")
+      .send({ password: "secret_admin", email: "userInvalid@user.com" });
+    expect(status).to.be.equal(401);
+    expect(body).to.be.deep.equal({ message: "Invalid email or password" });
+  });
 
-      it('Se os dados válidos deve passar', async () => {
-        const {status, body} = await chai.request(app).post('/login').send({password: 'secret_user', email: 'user@user.com'})
-        expect(status).to.be.equal(200)
-        expect(body).to.have.property('token').that.is.a('string')
-      });
+  it("Se o campo password for inválido deve falhar", async () => {
+    const { status, body } = await chai
+      .request(app)
+      .post("/login")
+      .send({ password: "invalid_password", email: "user@user.com" });
+    expect(status).to.be.equal(401);
+    expect(body).to.be.deep.equal({ message: "Invalid email or password" });
+  });
 
-      // it('testa ao enviar os dados corretos para a rota /login', async () => {
-      //   const {status, body} = await chai
-      //     .request(app)
-      //     .post('/login')
-      //     .send({
-      //       email: "admin@admin.com",
-      //       password: "secret_admin"
-      //     })
-      //     expect(status).to.be.equal(200);
-      //     expect(body).to.have.property('token').that.is.a('string');
-      // })
+  it("Deve retornar o token de acesso se os dados de login estiverem corretos", async () => {
+    const fakeUser = { email: "user@user.com", password: "secret_user" };
+    const secret = "jwt_secret";
+    const token = jwt.sign(fakeUser, secret);
+  
+    const fakeVerify = sinon.fake.returns(mockToken);
 
-      // it('Se não existir token ao acessar a rota "/login/role"', async () => {
-      //   const {status, body} = await chai.request(app).get('/login/role')
-      //   expect(status).to.be.equal(401)
-      //   expect(body).to.be.deep.equal({ "message": "Token not found" })
-      // });
+    
+    sinon.stub(jwt, "sign").callsFake(fakeVerify);
+    jwt.verify(token, secret);
 
-      // it('Se não existir token ao acessar a rota "/login/role"', async () => {
-      //   const {status, body} = await chai.request(app).get('/login/role').set('Authorization', 'invalidToken')
-      //   expect(status).to.be.equal(401)
-      //   expect(body).to.be.deep.equal({ "message": "Token not found" })
-      // });
+    // sinon.assert.calledOnce(fakeVerify);
+    // sinon.assert.calledWith(fakeVerify, token, secret);
+  
+    const { status, body } = await chai
+      .request(app)
+      .post("/login")
+      .send({ email: fakeUser.email, password: fakeUser.password });
+  
+    expect(status).to.be.equal(200);
+    expect(body).to.have.property("token").that.is.a("string");
+  
+  });
+  
 
+  it('Se não existir token ao acessar a rota "/login/role"', async () => {
+    const {status, body} = await chai.request(app).get('/login/role')
+    expect(status).to.be.equal(401)
+    expect(body).to.be.deep.equal({ "message": "Token not found" })
+  });
+
+
+  it('Se não existir token ao acessar a rota "/login/role"', async () => {
+    const {status, body} = await chai.request(app).get('/login/role')
+    expect(status).to.be.equal(401)
+    expect(body).to.be.deep.equal({role: "user"})
+  });
 
   /**
    * Exemplo do uso de stubs com tipos
@@ -89,5 +110,4 @@ describe('Teste da rota de Login', () => {
 
   //   expect(...)
   // });
-
 });
--- app/backend/src/tests/matchesIntegration.test.ts
+++ app/backend/src/tests/matchesIntegration.test.ts
@@ -1,5 +1,6 @@
 import * as sinon from "sinon";
 import * as chai from "chai";
+import * as jwt from "jsonwebtoken";
 // @ts-ignore
 import chaiHttp = require("chai-http");
 
@@ -13,119 +14,115 @@ const { expect } = chai;
 
 const mockMatches = [
   {
-    "id": 1,
-    "homeTeamId": 16,
-    "homeTeamGoals": 1,
-    "awayTeamId": 8,
-    "awayTeamGoals": 1,
-    "inProgress": false,
-    "homeTeam": {
-      "id": 16,
-      "teamName": "São Paulo"
+    id: 1,
+    homeTeamId: 16,
+    homeTeamGoals: 1,
+    awayTeamId: 8,
+    awayTeamGoals: 1,
+    inProgress: false,
+    homeTeam: {
+      id: 16,
+      teamName: "São Paulo",
     },
-    "awayTeam": {
-      "id": 8,
-      "teamName": "Grêmio"
-    }
+    awayTeam: {
+      id: 8,
+      teamName: "Grêmio",
+    },
   },
   {
-    "id": 2,
-    "homeTeamId": 9,
-    "homeTeamGoals": 1,
-    "awayTeamId": 14,
-    "awayTeamGoals": 1,
-    "inProgress": false,
-    "homeTeam": {
-      "id": 9,
-      "teamName": "Internacional"
+    id: 2,
+    homeTeamId: 9,
+    homeTeamGoals: 1,
+    awayTeamId: 14,
+    awayTeamGoals: 1,
+    inProgress: false,
+    homeTeam: {
+      id: 9,
+      teamName: "Internacional",
     },
-    "awayTeam": {
-      "id": 14,
-      "teamName": "Santos"
-    }
+    awayTeam: {
+      id: 14,
+      teamName: "Santos",
+    },
   },
   {
-    "id": 3,
-    "homeTeamId": 4,
-    "homeTeamGoals": 3,
-    "awayTeamId": 11,
-    "awayTeamGoals": 0,
-    "inProgress": false,
-    "homeTeam": {
-      "id": 4,
-      "teamName": "Corinthians"
+    id: 3,
+    homeTeamId: 4,
+    homeTeamGoals: 3,
+    awayTeamId: 11,
+    awayTeamGoals: 0,
+    inProgress: false,
+    homeTeam: {
+      id: 4,
+      teamName: "Corinthians",
     },
-    "awayTeam": {
-      "id": 11,
-      "teamName": "Napoli-SC"
-    }
+    awayTeam: {
+      id: 11,
+      teamName: "Napoli-SC",
+    },
   },
-  
-]
+];
 
 const matchesMockTrue = [
   {
-    "id": 1,
-    "homeTeamId": 16,
-    "homeTeamGoals": 1,
-    "awayTeamId": 8,
-    "awayTeamGoals": 1,
-    "inProgress": true,
-    "homeTeam": {
-      "id": 16,
-      "teamName": "São Paulo"
+    id: 1,
+    homeTeamId: 16,
+    homeTeamGoals: 1,
+    awayTeamId: 8,
+    awayTeamGoals: 1,
+    inProgress: true,
+    homeTeam: {
+      id: 16,
+      teamName: "São Paulo",
     },
-    "awayTeam": {
-      "id": 8,
-      "teamName": "Grêmio"
-    }
+    awayTeam: {
+      id: 8,
+      teamName: "Grêmio",
+    },
   },
   {
-    "id": 2,
-    "homeTeamId": 9,
-    "homeTeamGoals": 1,
-    "awayTeamId": 14,
-    "awayTeamGoals": 1,
-    "inProgress": true,
-    "homeTeam": {
-      "id": 9,
-      "teamName": "Internacional"
+    id: 2,
+    homeTeamId: 9,
+    homeTeamGoals: 1,
+    awayTeamId: 14,
+    awayTeamGoals: 1,
+    inProgress: true,
+    homeTeam: {
+      id: 9,
+      teamName: "Internacional",
     },
-    "awayTeam": {
-      "id": 14,
-      "teamName": "Santos"
-    }
+    awayTeam: {
+      id: 14,
+      teamName: "Santos",
+    },
   },
   {
-    "id": 3,
-    "homeTeamId": 4,
-    "homeTeamGoals": 3,
-    "awayTeamId": 11,
-    "awayTeamGoals": 0,
-    "inProgress": true,
-    "homeTeam": {
-      "id": 4,
-      "teamName": "Corinthians"
+    id: 3,
+    homeTeamId: 4,
+    homeTeamGoals: 3,
+    awayTeamId: 11,
+    awayTeamGoals: 0,
+    inProgress: true,
+    homeTeam: {
+      id: 4,
+      teamName: "Corinthians",
     },
-    "awayTeam": {
-      "id": 11,
-      "teamName": "Napoli-SC"
-    }
+    awayTeam: {
+      id: 11,
+      teamName: "Napoli-SC",
+    },
   },
-  
-]
+];
 
-// const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoyLCJ1c2VybmFtZSI6IlVzZXIiLCJyb2xlIjoidXNlciIsImVtYWlsIjoidXNlckB1c2VyLmNvbSIsInBhc3N3b3JkIjoiJDJhJDA4JFk4QWJpOGpYdnNYeXFtLnJtcDBCLnVRQkE1cVV6N1Q2R2hsZy9DdlZyL2dMeFlqNVVBWlZPIn0sImlhdCI6MTY4MzUwMzMwOSwiZXhwIjoxNjg0MTA4MTA5fQ.CRTkbGP6kxkoCZvPHwgttfzWCzTbF8Y95iIGKBnWU2E'
-  
+const mockToken =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoyLCJ1c2VybmFtZSI6IlVzZXIiLCJyb2xlIjoidXNlciIsImVtYWlsIjoidXNlckB1c2VyLmNvbSIsInBhc3N3b3JkIjoiJDJhJDA4JFk4QWJpOGpYdnNYeXFtLnJtcDBCLnVRQkE1cVV6N1Q2R2hsZy9DdlZyL2dMeFlqNVVBWlZPIn0sImlhdCI6MTY4MzU1NDI3MywiZXhwIjoxNjg0MTU5MDczfQ.0cYBEnAPk4iSf0DMqtu-_wEERmUSppQjQiLj533jxuw";
 
 describe("Teste da rota de Matches", () => {
   afterEach(sinon.restore);
 
   it("A requisição deve retornar todos as partidas", async () => {
     sinon.stub(Model, "findAll").resolves(mockMatches as any);
-    const { status, body } = await chai
-      .request(app)
-      .get("/matches")
+    const { status, body } = await chai.request(app).get("/matches");
     expect(status).to.be.equal(200);
     expect(body).to.be.deep.equal(mockMatches);
   });
@@ -135,57 +132,68 @@ describe("Teste da rota de Matches", () => {
     const { status, body } = await chai
       .request(app)
       .get("/matches")
-      .query({inProgress: true})
+      .query({ inProgress: true });
     expect(status).to.be.equal(200);
     expect(body).to.be.deep.equal(matchesMockTrue);
   });
 
-  it("A requisição atualizar uma partida pelo id", async () => {
+  it("A requisição deve atualizar uma partida pelo id", async () => {
     const update = {
-      "id": 1,
-      "homeTeamId": 16,
-      "homeTeamGoals": 1,
-      "awayTeamId": 8,
-      "awayTeamGoals": 1,
-      "inProgress": false
+      id: 1,
+      homeTeamId: 16,
+      homeTeamGoals: 1,
+      awayTeamId: 8,
+      awayTeamGoals: 1,
+      inProgress: true,
+      homeTeam: { id: 16, teamName: 'São Paulo' },
+      awayTeam: { id: 8, teamName: 'Grêmio' }
     }
-    
-    sinon.stub(Model, "update").resolves(update as any);
-    sinon.stub(Model, "findByPk").resolves(matchesMockTrue[0] as any);
-    const response = await chai
-    .request(app)
-    .post('/login')
-    .send({
+    const secret = "jwt_secret";
+    const fakeVerify = sinon.fake.returns({
       "email": "user@user.com",
       "password": "secret_user"
-    })
+    });
+    sinon.stub(jwt, 'verify').callsFake(fakeVerify);
+    jwt.verify(mockToken, secret);
+    sinon.assert.calledOnce(fakeVerify);
+    sinon.assert.calledWith(fakeVerify, mockToken, secret);
 
-    const {token} = response.body
-    console.log(token);
-    
+    sinon.stub(Model, "update").resolves(update as any);
+    sinon.stub(Model, "findByPk").resolves(matchesMockTrue[0] as any);
 
     const { status, body } = await chai
       .request(app)
       .patch("/matches/1")
-      .set("Authorization", token)
-       .send({
+      .set("Authorization", mockToken)
+      .send({
         homeTeamGoals: 1,
-        awayTeamGoals: 1
-      })
+        awayTeamGoals: 1,
+      });
+
     expect(status).to.be.equal(200);
     expect(body).to.be.deep.equal(update);
   });
 
   // it("A requisição deve retornar a partida pelo id", async () => {
+  //   const mockResponse = {
+  //     "id": 1,
+  //     "homeTeamId": 16,
+  //     "homeTeamGoals": 1,
+  //     "awayTeamId": 8,
+  //     "awayTeamGoals": 1,
+  //     "inProgress": false
+  //   }
+    
   //   sinon.stub(Model, "findByPk").resolves(matchesMockTrue[0] as any);
   //   const { status, body } = await chai
   //     .request(app)
   //     .get("/matches/1")
-  //     .set("Authorization", token)
+  //     .set("Authorization", mockToken)
+  //     console.log(status, body);
+      
   //   expect(status).to.be.equal(200);
   //   expect(body).to.be.deep.equal(matchesMockTrue[0]);
   // });
-  
 
   /**
    * Exemplo do uso de stubs com tipos
